0408b4504d3b844af2420ed19bdb8172e45c5128
kamaboko <95174703+kamaboko6214@users.noreply.github.com>
2023-08-23 21:35:14 +0900

    add:作成画面

--- laravel-project/resources/js/index.js
+++ laravel-project/resources/js/index.js
@@ -1,14 +1,32 @@
 window.add_card = () => {
-    let card = document.getElementById("card");
+    let overview = document.getElementById("overview")
+    let card = document.getElementById("card_content")
     let clone_card = card.cloneNode(true)
+    let card_id = card.id
+    let clone_card_id = card_id + (new Date()).getTime()
+    clone_card.id = clone_card_id
+    let title = document.getElementById("view_title")
+    title.value = ''
+    let body = document.getElementById("view_body")
+    body.value = ''
+    overview.appendChild(clone_card);
 
-    if(card.id == 'card') {
-        clone_card.id = "card2"
-    } else {
-        let card_id = card.id.slice(-1)
-        let clone_id = "card" + card_id + 1
-        clone_card.id = clone_id
-    }
-
-    card.after(clone_card);
 }
+
+window.remove_card = (e) => {
+    var e = e || window.event;
+    var elem = e.target || e.srcElement;
+    var elemId = elem.id;
+    let parent2 = elem.parentNode
+    let parent = parent2.parentNode
+    if (parent.id === 'card_content') {
+        alert('これ以上削除できません。')
+        return false;
+    }
+    if(confirm('削除しますか？')){
+        parent.remove()
+    }
+};
+
+
+
